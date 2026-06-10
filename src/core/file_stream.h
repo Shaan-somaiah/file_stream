cd2ee60416d9602b062cc357de9038533138f5ad
Shaan-somaiah <shaansomaiah@gmail.com>
2026-06-10 22:51:39 +0530

    Initial FileEngine class body
    Defined initial body for FileEngine class without any implementation

--- src/core/file_stream.h
+++ src/core/file_stream.h
@@ -1,7 +1,35 @@
 #ifndef FILE_STREAM_H
 #define FILE_STREAM_H
 
+#include <gflags/gflags.h>
+#include <filesystem>
+
+DECLARE_string(source_file);
+DECLARE_string(destination_file);
+DECLARE_bool(overwrite);
+
 namespace file_stream {
+    class FileEngine {
+        public:
+
+            FileEngine() = delete;
+
+            FileEngine(const std::filesystem::path& source_file, const std::filesystem::path& destination_file);
+
+            void initialize();
+
+            bool validateFileExists(const std::filesystem::path file);
+
+            bool fileStream(const std::filesystem::path& source_file, const std::filesystem::path& destination_file);
+            
+            bool overwriteFileStream(const std::filesystem::path& source_file, const std::filesystem::path& destination_file);
+
+            bool createFile(const std::filesystem::path& file);
+
+        private:
+            const std::filesystem::path m_source_file;
+            const std::filesystem::path m_destination_file;
+    };
 
 } // namespace file_stream
 
